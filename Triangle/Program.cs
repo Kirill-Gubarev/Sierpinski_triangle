f0a04410e2132779a160c49b3b55a8bb75d06e3f
Kirill <127307261+Kirill-Gubarev@users.noreply.github.com>
2023-03-13 21:43:26 +0700

    Update Program.cs

--- Triangle/Program.cs
+++ Triangle/Program.cs
@@ -12,19 +12,19 @@
     }
     static void FFF(short n, short x, short y)
     {
-        if (n == 0)
+        if (n == 0)//
         {
-            Thread.Sleep(1);
+            Thread.Sleep(30);
             Cell(x, y);
         }
-        else
+        else//making 3 copies
         {
             FFF((short)(n / 2), x, y);
             FFF((short)(n / 2), (short)(x + n), y);
             FFF((short)(n / 2), x, (short)(y + n));
         }
     }
-    static void Cell(short x, short y)
+    static void Cell(short x, short y)//cell rendering
     {
         Console.SetCursorPosition(x * 2, y);
         Console.Write('█');
