f0b9768091d793921d59ed9a895daea380082536
Kirill <kirillgubarev5668@mail.ru>
2023-03-13 21:36:56 +0700

    first commit

---
+++ Triangle/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        byte.TryParse(Console.ReadLine(), out byte k);
+        short n = (short)(Math.Pow(2, k));
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Green;
+        short x = 0, y = 0;
+        FFF(n, x, y);
+        Console.ReadLine();
+    }
+    static void FFF(short n, short x, short y)
+    {
+        if (n == 0)
+        {
+            Thread.Sleep(1);
+            Cell(x, y);
+        }
+        else
+        {
+            FFF((short)(n / 2), x, y);
+            FFF((short)(n / 2), (short)(x + n), y);
+            FFF((short)(n / 2), x, (short)(y + n));
+        }
+    }
+    static void Cell(short x, short y)
+    {
+        Console.SetCursorPosition(x * 2, y);
+        Console.Write('█');
+        Console.SetCursorPosition(x * 2 + 1, y);
+        Console.Write('█');
+    }
+}
